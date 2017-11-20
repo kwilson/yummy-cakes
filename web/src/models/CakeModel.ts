4a9d7d1e0b383d290a022167adcc5e181f4def59
Kevin Wilson <kevin@partcoffee.com>
2017-11-20 18:22:59 +0000

    Add tests for Cake component

--- web/src/models/CakeModel.ts
+++ web/src/models/CakeModel.ts
@@ -1,7 +1,7 @@
 export interface CakeModel {
   id: string;
-  name: string;
-  imageUrl: string;
-  yumFactor: number;
-  comment: string;
+  name?: string;
+  imageUrl?: string;
+  yumFactor?: number;
+  comment?: string;
 }
