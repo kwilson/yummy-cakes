4d006b11f1474b2dc1dba7e8e616ccb13e9761fe
Kevin Wilson <kevin.wilson@bjss.com>
2017-11-20 14:20:34 +0000

    Update API endpoint to correct one

--- web/src/reducers/rest.ts
+++ web/src/reducers/rest.ts
@@ -2,6 +2,6 @@ const reduxApi = require('redux-api');
 const adapterFetch = require('redux-api/lib/adapters/fetch');
 
 export default reduxApi({
-  cakes: `http://localhost:3001/cakes/:id`
+  cakes: `http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com/api/cakes/:id`
 })
 .use('fetch', adapterFetch(fetch));
