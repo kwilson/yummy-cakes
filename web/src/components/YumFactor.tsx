62102322cc6ce3d68ff36d91299fa21e3d8dcb0f
Kevin Wilson <kevin@partcoffee.com>
2017-11-20 17:13:53 +0000

    Add YumFactor component

--- web/src/components/YumFactor.tsx
+++ web/src/components/YumFactor.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import './YumFactor.css';
 
 export interface YumFactorStateProps {
   value: number;
 }
 
 const YumFactor: React.StatelessComponent<YumFactorStateProps> = (props) => {
+  const ratingLabel = `${props.value} out of 5`;
+  const stars = '☆☆☆☆☆'.substr(0, props.value);
+
   return (
-    <p>YUM: {props.value}</p>
+    <div className="yum-factor">
+      Yum Factor
+      
+      <div className="yum-factor__rating" title={ratingLabel} aria-label={ratingLabel}>
+        {stars}
+      </div>
+    </div>
   );
 };
 
